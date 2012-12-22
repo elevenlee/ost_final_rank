97375ab3a0ffb36fd5fb794e22c7fa63feb610a8
Eleven Lee <sl3268@nyu.edu>
2012-12-22 11:17:49 -0500

    Add python doc to rankdata.py and category.py

--- category.py
+++ category.py
@@ -21,22 +21,60 @@ delete_action_path = 'category/deleteaction'
 item_page_path = 'item/addeditdelete'
 
 def get_category(author, name):
+    """Returns category of the specified author, and name
+
+    :param author: the specified category author
+    :param name: the specified category name
+    :returns: category of the specified author and name
+    """
     category_key = db.Key.from_path('Category', '{author}/{category}'.format(author=author, category=name))
     return db.get(category_key)
 
 def get_category_by_key(key):
+    """Returns category of specified key
+
+    :param key the specified key
+    :returns: category of the specified key
+    """
     category_key = db.Key.from_path('Category', key)
     return db.get(category_key)
 
 def get_categories_by_name(keyword):
+    """Returns categories of specified keyword
+
+    Retrive all categories that its name equals keyword
+    :param: keyword the specified keyword
+    :returns: categories of specified keyword
+    """
     category_query = rankdata.Category.all().filter('name = ', keyword)
     return category_query.fetch(limit=None)
 
 def get_categories(author=None, order='-create_time', item_number=0):
+    """Returns categories of specified author, order and the number of
+       item in category is equals to or greater than the specified item
+       number
+
+    :param author the specified author. The default value is None,
+                  if so, the all categories would be returned
+    :param order the specified order. The default value is ordering by
+                 create time field in descending
+    :param item_number the specified item number. The default value is 0
+    :returns: categories of specified author, order and the number of
+              item in category is euqlas to or greater than the specified
+              item number
+    """
     category_query = rankdata.Category.all().filter("author = ", author).order(order) if author else rankdata.Category.all().order(order)
     return [ category for category in category_query.run() if item.get_items(author=category.author, category_name=category.name, count_or_not=True) >= item_number ] if item_number else category_query.run()
 
 def update_category(author, new_name, old_name):
+    """Update category of specified author, old name, and new name
+
+    When update the category name, all items belonging to as well as
+    its all comments would be reserved automatically.
+    :param author the specified author
+    :param new_name the specified new category name
+    :param old_name the specified old category name
+    """
     new_category = rankdata.Category(key_name='{author}/{name}'.format(author=author, name=new_name), author=author, name=new_name)
     new_category.put()
     item.reserve_all_items(author=author, new_category_name=new_name, old_category_name=old_name)
@@ -44,19 +82,40 @@ def update_category(author, new_name, old_name):
     db.delete(old_category)
 
 def delete_categories(author, names=[]):
+    """Delete categories of specified author and name
+
+    When delete a category, all items belonging to as well as its all
+    comments would be delete automatically.
+    :param author the specified author
+    :param names the specified name list
+    """
     for name in names:
         key = db.Key.from_path('Category', '{author}/{category}'.format(author=author, category=name))
         item.delete_all_items(author, db.get(key).name)
         db.delete(key)
 
 def delete_all_categories(author):
+    """Delete all categories of specified author
+
+    When delete a category, all items belonging to as well a its all
+    comments would be delete automatically
+    :param author the specified author
+    """
     category_query = rankdata.Category.all().filter("author = ", author)
     for category in category_query.run():
         item.delete_all_items(author, category.name)
         db.delete(category)
 
 class AddCategoryPage(webapp2.RequestHandler):
+    """Construct add category HTML page
+
+    """
     def get(self):
+        """Handle user request
+
+        The add category HTML page would list all categories that the
+        user already have.
+        """
         invalid_name = self.request.get('invalid_name')
         user = users.get_current_user()
         categories = get_categories(author=user)
@@ -72,7 +131,15 @@ class AddCategoryPage(webapp2.RequestHandler):
         self.response.out.write(template.render(template_values))
 
 class AddCategoryAction(webapp2.RequestHandler):
+    """Handle add category form submition
+
+    """
     def post(self):
+        """Handle user request
+
+        The category name could not be empty, comtains '/' character as
+        well as already exist.
+        """
         name = self.request.get('category_name').strip()
         if name == '':
             self.redirect('/{path}?'.format(path=add_page_path) +
@@ -94,7 +161,15 @@ class AddCategoryAction(webapp2.RequestHandler):
                           urllib.urlencode({'category_name': name, 'method': 'Add'}))
 
 class EditCategoryPage(webapp2.RequestHandler):
+    """Construct edit category HTML page
+
+    """
     def get(self):
+        """Handle user request
+
+        The edit category HTML page would list all categories that user
+        already have.
+        """
         invalid_select = self.request.get('select_category')
         user = users.get_current_user()
         categories = get_categories(author=user)
@@ -110,7 +185,16 @@ class EditCategoryPage(webapp2.RequestHandler):
         self.response.out.write(template.render(template_values))
 
 class EditCategoryAction(webapp2.RequestHandler):
+    """Handle edit category form submition and construct edit category
+       name HTML page
+
+    """
     def get(self):
+        """Handler user request
+
+        The edit category name HTML page would list all items belonging to
+        the specified category.
+        """
         category_name = self.request.get('category_name')
         if not category_name:
             self.redirect('/{path}?'.format(path=edit_page_path) +
@@ -133,7 +217,17 @@ class EditCategoryAction(webapp2.RequestHandler):
         self.response.out.write(template.render(template_values))
 
 class EditCategoryNameAction(webapp2.RequestHandler):
+    """Handle edit category name form submition
+
+    """
     def post(self):
+        """Handler user request
+
+        The new category name would not be empty, contains '/' character as
+        well as already exist. If the user does not change the category name,
+        it will do nothing. Otherwise, all items belonging to as well as its
+        all comments would be reserved automatically.
+        """
         old_category_name = self.request.get('category_name').strip()
         new_category_name = self.request.get('new_category_name').strip()
         if not cmp(old_category_name, new_category_name):
@@ -160,7 +254,15 @@ class EditCategoryNameAction(webapp2.RequestHandler):
                           urllib.urlencode({'category_name': new_category_name}))
 
 class DeleteCategoryPage(webapp2.RequestHandler):
+    """Construct delete category HTML page
+
+    """
     def get(self):
+        """Handle user request
+
+        The delete category HTML page would list all categories that user
+        already have as well as categories that user just deleted
+        """
         delete_name = self.request.get('delete_name')
         delete_names = util.parse_string(delete_name) if delete_name else None
         user = users.get_current_user()
@@ -177,7 +279,13 @@ class DeleteCategoryPage(webapp2.RequestHandler):
         self.response.out.write(template.render(template_value))
 
 class DeleteCategoryAction(webapp2.RequestHandler):
+    """Handle delete category form submition
+
+    """
     def post(self):
+        """Handle user request
+
+        """
         command = self.request.get('delete')
         user = users.get_current_user()
         url = users.create_logout_url(self.request.uri)
--- rankdata.py
+++ rankdata.py
@@ -4,12 +4,37 @@ from google.appengine.ext import db
 from google.appengine.api import users
 
 class Category(db.Model):
+    """Store category information
+
+    The category information includes four fields: author, name,
+    create time, and last modified time. The author as well as name
+    fields are required.
+
+    When store a new category information, the author field would be
+    assigned to the current user automatically. Also, the create time
+    field would be assigned to the current time automatically only the
+    new category add to the datastore other than updating. In a dissimilar
+    way, the last modified time field would be assigned to the current
+    time automatically at every updating.
+    """
     author = db.UserProperty(required=True, auto_current_user_add=True)
     name = db.StringProperty(required=True)
     create_time = db.DateTimeProperty(auto_now_add=True)
     last_modified_time = db.DateTimeProperty(auto_now=True)
 
 class Item(db.Model):
+    """Store item information
+
+    The item information includes six fields: name, create time, last
+    modified time, number of win, number of lose, and percentage. The
+    name, number of win, number of lose, as well as percentage fields
+    are required.
+
+    Each item must have a parent indicated the category it belonged to.
+    When add a new item information, the create time field would be
+    assigned to the current time automatically. The last modified time
+    field would be assigned to the current time at every updating.
+    """
     name = db.StringProperty(required=True)
     create_time = db.DateTimeProperty(auto_now_add=True)
     last_modified_time = db.DateTimeProperty(auto_now=True)
@@ -18,6 +43,18 @@ class Item(db.Model):
     percentage = db.FloatProperty(required=True)
 
 class Comment(db.Model):
+    """Store comment information
+
+    The comment information includes three fields, commenter, content,
+    and submit time. The name as well as content fields are required.
+
+    Each comment must have a parent indicated the item it belonged to.
+    When add a new comment information, the commentor would be assigned
+    to the current user automatically. In a not dissimilar way, the
+    submit time field would be assigned to the current time automatically.
+    The comment information is immutable. In other words, once comment
+    add to the datastore, it could not be changed ever.
+    """
     commenter = db.UserProperty(required=True, auto_current_user_add=True)
     content = db.StringProperty(required=True, multiline=True)
     submit_time = db.DateTimeProperty(auto_now_add=True)
